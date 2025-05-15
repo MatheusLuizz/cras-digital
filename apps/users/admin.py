875df29ec690146bd88c45ce27ff757599c49e9a
MatheusLuizz <mlfs7@discente.ifpe.edu.br>
2025-05-15 10:46:59 -0300

    feature: Add Jazzmin theme to Django admin for improved UI

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,4 +1,42 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
 from .models import User
 
-admin.site.register(User)
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('username', 'cpf', 'email')
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('username', 'cpf', 'email')
+
+
+class UserAdmin(DjangoUserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ['username', 'email', 'cpf', 'is_staff']
+
+    # Sobrescreve fieldsets sem 'usable_password'
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'cpf', 'document')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'cpf', 'email', 'password1', 'password2'),
+        }),
+    )
+
+
+admin.site.register(User, UserAdmin)
