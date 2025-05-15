239a162bf97e19fccd1150b4deb4676466563a49
MatheusLuizz <mlfs7@discente.ifpe.edu.br>
2025-05-15 09:50:13 -0300

    lint: apply flake8 linting to enforce code style guidelines

--- apps/users/views.py
+++ apps/users/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import AllowAny
 from .models import User
 from .serializers import UserSerializer
 
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
