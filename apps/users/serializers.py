4ef89322a112db587773b5c0a9bcb426aec73551
MatheusLuizz <mlfs7@discente.ifpe.edu.br>
2025-05-17 10:15:46 -0300

    feature: add support messages & tickets feature

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,21 +1,33 @@
 from rest_framework import serializers
-from .models import User
-from django.contrib.auth.password_validation import validate_password
+from .models import User, UserProfile
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = '__all__'
 
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    profile = UserProfileSerializer(required=False)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'cpf', 'document', 'password']
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'cpf', 'document', 'profile', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password'],
-            cpf=validated_data.get('cpf'),
-            document=validated_data.get('document')
-        )
+        profile_data = validated_data.pop('profile', None)
+        password = validated_data.pop('password', None)
+
+        user = User.objects.create(**validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+
+        if profile_data:
+            UserProfile.objects.create(user=user, **profile_data)
+        else:
+            UserProfile.objects.create(user=user)
+
         return user
